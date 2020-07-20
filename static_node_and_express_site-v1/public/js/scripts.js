cd4434304a619efedebe68d3d038f07eeb3ac328
Iqbal955 <58709666+Iqbal955@users.noreply.github.com>
2020-07-20 17:59:18 +0200

    test

--- static_node_and_express_site-v1/public/js/scripts.js
+++ static_node_and_express_site-v1/public/js/scripts.js
@@ -1,1 +1,31 @@
-$(document).foundation()
+//$(document).foundation()
+const express = require('express');
+const app = express();
+
+
+app.engine('pug', require('pug').__express)
+
+app.set('view engine', 'pug');
+
+app.listen(3000, () => {
+    console.log("Bi Boooob bap connection is there")
+});
+
+
+
+
+app.get('/', (req, res) => {
+
+   res.render('<h1>Mic check 123</>');
+
+});
+
+
+app.get('/about', (req, res) => {
+
+    res.render('index.pug');
+
+
+
+});
+
